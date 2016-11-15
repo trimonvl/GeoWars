a150ee1a1fa1954797ff18552821c741b20a6c93
Jonas-lauwers <jonas.lauwers@gmail.com>
2016-11-15 20:08:46 +0200

    add function to run temp test

--- src/be/howest/groep12/geowars/model/BulletList.java
+++ src/be/howest/groep12/geowars/model/BulletList.java
@@ -4,10 +4,18 @@ import java.awt.Graphics;
 import java.util.ArrayList;
 
 public class BulletList {
-	ArrayList<Bullet> bulletList = new ArrayList<>(); 
+    
+	private ArrayList<Bullet> bulletList = new ArrayList<>(); 
+        
 	public void add(Bullet bullet){
 		bulletList.add(bullet);
 	}
+        
+        //test for bullets
+        public int getNumber() {
+            return bulletList.size();
+        }
+        
 	public void draw(Graphics g){
 		for(int i=0;i<bulletList.size();i++){
 			bulletList.get(i).draw(g);
