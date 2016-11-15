f42d2f380b4e39e665daefc72c3b7ff9f555596e
Jonas-lauwers <jonas.lauwers@gmail.com>
2016-11-15 20:09:34 +0200

    very basic engine to start

---
+++ src/be/howest/groep12/geowars/model/GameEngine.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package be.howest.groep12.geowars.model;
+
+import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Jonas Lauwers
+ */
+public class GameEngine {
+    
+   private Ship ship;
+   private List<Enemy> enemies;
+   private BulletList bullets;
+   
+   //TODO empty construct, set ship and set difficulty?
+   public GameEngine(Ship ship) {
+       this.ship = ship;
+       enemies = new ArrayList<>();
+       bullets = new BulletList();
+   }
+   
+   //moves player (-)x, (-)y coords
+   public void movePlayer(int x, int y) {
+       ship.setxPos(ship.getxPos()+x);
+       ship.setyPos(ship.getyPos()+y);
+   }
+   
+   //test for playerpos
+   public int[] getPlayerPos() {
+       int[] coord = new int[2];
+       coord[0] = ship.getxPos();
+       coord[1] = ship.getyPos();
+       return coord;
+   }
+   
+   //test for bullets
+   public int getNumberOfBullets() {
+       return bullets.getNumber();
+   }
+   
+   public void playerShoot() {
+       ship.shoot(bullets);
+   }
+   
+   //test for enemies
+   public int getNumberOfEnemies() {
+       return enemies.size();
+   }
+   
+   public void addEnemy(Enemy e) {
+       enemies.add(e);
+   }
+   
+   //TODO make enemy move to player ....
+   //will be done with lib I guess, same for player.
+   
+   public void draw(Graphics g) {
+       ship.draw(g);
+       for(Enemy e : enemies) {
+           e.draw(g);
+       }
+       bullets.draw(g);
+   }
+}
